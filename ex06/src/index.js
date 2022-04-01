cda62eccd122ec17fe22111cc1cebcb8ce80a730
Sumejja Fazlic <sumejjafazlic@hotmail.com>
2022-04-01 15:43:14 +0200

    Add Exercise 06

---
+++ ex06/src/index.js
@@ -0,0 +1,9 @@
+function main(){
+    var myString = "Concatenation is the process of appending ";
+    myString += "one string to the end of another string."
+    return myString;
+    
+    }
+    
+    console.log(main());
+    module.exports = main;
