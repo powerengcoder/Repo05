fb03477bcf026f1ec593fca632c4252afbc327c9
Sumejja Fazlic <sumejjafazlic@hotmail.com>
2022-04-01 16:27:38 +0200

    Add Exercise 09

---
+++ ex09/src/index.js
@@ -0,0 +1,10 @@
+function main(){
+    var lasName = "Doe";
+    var lastNameLength;
+    lastNameLength = lasName.length;
+
+    return lastNameLength;
+    }
+    
+    console.log(main());
+    module.exports = main;
