46573398ac976108e44438ebe19a022c0ed43603
Sumejja Fazlic <sumejjafazlic@hotmail.com>
2022-04-01 16:55:59 +0200

    Add Exercise 12

---
+++ ex12/src/index.js
@@ -0,0 +1,10 @@
+function main(){
+    var lastName = "Tesla";
+    var thirdLetterOfLastName = "";
+    thirdLetterOfLastName = lastName[2];
+
+    return thirdLetterOfLastName;
+    }
+    
+    console.log(main());
+    module.exports = main;
