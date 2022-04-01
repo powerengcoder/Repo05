56dad8199d249d99cd2d884d7cd79b5cbe9501a9
Sumejja Fazlic <sumejjafazlic@hotmail.com>
2022-04-01 15:49:04 +0200

    Add Exercise 07

---
+++ ex07/src/index.js
@@ -0,0 +1,9 @@
+function main(){
+    var myName = "Sumejja";
+    var myString = "My name is " + myName + " and I am well!";
+    return myString;
+    
+    }
+    
+    console.log(main());
+    module.exports = main;
