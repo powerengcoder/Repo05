136b85de91dbcc0041d51c490648e025ebc1b7e7
Sumejja Fazlic <sumejjafazlic@hotmail.com>
2022-04-01 22:02:01 +0200

    Add all exercises

---
+++ ex21/src/proba.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+  <body>
+    <main>
+      <h2>CatPhotoApp</h2>
+
+      <p>Kitty ipsum dolor sit amet, shed everywhere shed everywhere stretching attack your ankles chase the red dot, hairball run catnip eat the grass sniff.</p>
+      <p>Purr jump eat the grass rip the couch scratched sunbathe, shed everywhere rip the couch sleep in the sink fluffy fur catnip scratched.</p>
+
+      <img src="https://images.pexels.com/photos/1693441/pexels-photo-1693441.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="Gray cat on tree branch." />
+
+      <a href="https://www.pexels.com/search/cat/" target="_blank">cat photos</a>
+    </main>
+  </body>
+</html>
