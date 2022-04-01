d6a11e5162f6a00c690c164940fdd16375d3b508
Sumejja Fazlic <sumejjafazlic@hotmail.com>
2022-04-01 15:58:16 +0200

    Add Exercise 08

---
+++ ex08/src/index.js
@@ -0,0 +1,10 @@
+function main(){
+    var someAdjective = "the best!";
+    var myString = "Mistral is ";
+    myString +=someAdjective;
+    return myString;
+    
+    }
+    
+    console.log(main());
+    module.exports = main;
